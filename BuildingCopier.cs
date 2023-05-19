412fef8f9c66f35a09f96e320b98aa1ab208ea92
ats-mods <matty_119@hotmail.com>
2023-05-19 17:52:00 +0200

    block copying input if not idle

--- BuildingCopier.cs
+++ BuildingCopier.cs
@@ -1,5 +1,10 @@
+using System.Linq;
 using Eremite;
+using Eremite.Buildings;
+using Eremite.Buildings.UI;
 using Eremite.MapObjects;
+using Eremite.Services;
+using Eremite.View;
 
 namespace Stormwalker {
 
@@ -9,19 +14,50 @@ namespace Stormwalker {
             if(!Eremite.Controller.GameController.IsGameActive)
                 return;
 
-            if (MB.InputService.WasTriggered(MB.InputConfig.CopyBuilding, false))
-			{
-				var deposit = GameInputService.MouseoverObject.Value as ResourceDeposit;
-                if(deposit != null && deposit.State.isAvailable && DepositsService.HutsMatrix.ContainsKey(deposit.Model)){
-                    foreach (var building in DepositsService.HutsMatrix[deposit.Model])
-                    {
-                        if(GameContentService.IsUnlocked(building) && ConstructionService.CanConstruct(building)){
-                            GameBlackboardService.BuildingConstructionRequested.OnNext(building);
-                            break;
+            if(ShouldIgnoreInput())
+                return;
+
+            if (!InputService.WasTriggered(MB.InputConfig.CopyBuilding, false))
+                return;
+
+            var mapObject = GameInputService.MouseoverObject.Value;
+            Plugin.Log(mapObject);
+            switch(mapObject){
+                case ResourceDeposit deposit:
+                    if(deposit.State.isAvailable && DepositsService.HutsMatrix.ContainsKey(deposit.Model)){
+                        foreach(var building in DepositsService.HutsMatrix[deposit.Model]){
+                            if(BuildConditional(building)) break;
                         }
                     }
-                }
-			}
+                    break;
+                case NaturalResource resource when resource.State.isAvailable:
+                    BuildConditional(Settings.GetBuilding("Woodcutters Camp"));
+                    break;
+                case Spring spring when spring.State.isAvailable:
+                    BuildConditional(Settings.rainpunkConfig.extractor);
+                    break;
+                case Field field:
+                    if(OreService.HasAvailableOre(field.Pos)){
+                        var ore = OreService.GetOre(field.Pos);
+                        foreach(var building in OreService.MinesMatrix[ore.Model.refGood.Name]){
+                            if(BuildConditional(building)) break;
+                        }
+                    }
+                    break;
+            }
+        }
+
+        private bool ShouldIgnoreInput()
+		{
+			return BuildingPanel.currentBuilding != null || !ModeService.Idle.Value || InputService.IsOverUI();
+		}
+
+        private bool BuildConditional(BuildingModel building){
+            if(GameContentService.IsUnlocked(building) && ConstructionService.CanConstruct(building)){
+                GameBlackboardService.BuildingConstructionRequested.OnNext(building);
+                return true;
+            }
+            return false;
         }
     }
 }
